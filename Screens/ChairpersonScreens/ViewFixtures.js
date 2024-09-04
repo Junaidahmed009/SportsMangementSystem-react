30196af1c632ae9a939e8dbcc3a93ce2ce1ba9b9
JunaidAhmed <jr124880@gmail.com>
2024-09-05 01:31:13 +0500

    view fixture screen added

---
+++ Screens/ChairpersonScreens/ViewFixtures.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ViewFixtures() {
+  return (
+    <View>
+      <Text>ViewFixtures</Text>
+    </View>
+  )
+}
